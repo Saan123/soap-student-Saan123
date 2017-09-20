8a4f50ee7372bbd64d5c1221cf788395ed4cfe58
Sarasaan Sivaguru <sara360e@edu.easj.dk>
2017-09-20 12:38:31 +0200

    Implementeret Student klasse

---
+++ Soap Service Students/Soap Service Students/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Soap_Service_Students
+{
+	[DataContract]
+	public class Student
+	{
+		[DataMember]
+		public string Name { get; set; }
+
+		[DataMember]
+		public int ID { get; set; }
+	}
+}
